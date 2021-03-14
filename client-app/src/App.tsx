633df9ad920f5f1612921475b013e886e3c21919
EvgenyGuguev <evgenygost@gmail.com>
2021-03-14 14:39:36 +0300

    install react semantic ui

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import {Header, List} from "semantic-ui-react";
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -9,23 +9,20 @@ function App() {
   useEffect(() => {
     axios.get('https://localhost:5001/api/activities')
         .then(response => {
-          console.log(response);
           setActivities(response.data);
         })
   }, []);
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
           {activities.map((activity: any) => (
-              <li key={activity.id}>
-                {activity.title}
-              </li>
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
           ))}
-        </ul>
-      </header>
+        </List>
     </div>
   );
 }
