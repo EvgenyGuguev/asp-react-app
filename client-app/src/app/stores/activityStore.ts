c2084781a62c8990c6aefd979e6e77c249bd0c36
EvgenyGuguev <evgenygost@gmail.com>
2021-03-23 21:17:12 +0300

    react-router params

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -34,6 +34,7 @@ export default class ActivityStore {
   }
 
   loadActivities = async () => {
+    this.loadingInitial = true;
     try {
       const activities = await agent.Activities.list();
       activities.forEach(activity => {
@@ -55,6 +56,7 @@ export default class ActivityStore {
       try {
         activity = await agent.Activities.details(id);
         this.setActivity(activity);
+        this.selectedActivity = activity;
         this.setLoadingInitial(false);
       } catch (error) {
         console.log(error);
