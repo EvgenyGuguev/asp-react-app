a5605fb831cf8b73a8419725ea3160ca687a0374
EvgenyGuguev <evgenygost@gmail.com>
2021-04-14 21:10:55 +0300

    add popOver for attendee

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -18,7 +18,7 @@ export default function ActivityListItem({activity}: Props) {
         : null}
         <Item.Group>
           <Item>
-            <Item.Image size='tiny' circular src='/assets/user.png' />
+            <Item.Image style={{marginBottom: 3}} size='tiny' circular src='/assets/user.png' />
             <Item.Content>
               <Item.Header as={Link} to={`/activities/${activity.id}`}>
                 {activity.title}
